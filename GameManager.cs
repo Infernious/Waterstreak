0f0d1b0819828d63bdd4ccf09be821d63b44fedd
Andrew Bustos <infernious@live.com>
2018-11-20 16:26:59 -0800

    Adding c# files

---
+++ GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    [HideInInspector]
+    public bool groundHit = false;
+
+    bool printDone;
+
+    bool gameWon = false;
+
+    #region Singleton
+
+    public static GameManager instance;
+
+    void Awake()
+    {
+        printDone = false;
+        instance = this;
+    }
+
+    #endregion
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (groundHit && !printDone)
+        {
+            print("Game Over...");
+            printDone = true;
+        }
+	}
+
+    public void EndGame()
+    {
+        if (gameWon == false)
+        {
+            gameWon = true;
+
+           //TODO add game over mechanic 
+        }
+    }
+}
---
+++ LevelEnd.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEnd : MonoBehaviour {
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            GameManager.instance.EndGame();
+        }
+    }
+}
